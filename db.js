ff96af73cf84f5539ff6294a55cfe1ef5449adf2
dhumass02-ops <dhumass02@gmail.com>
2026-08-17 20:11:03 +0000

    Create db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+pool.on('error', (err) => {
+    console.error('Erreur inattendue sur le client PostgreSQL :', err);
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+};
